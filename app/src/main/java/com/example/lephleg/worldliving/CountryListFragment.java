6ab27c44ea9e8a6fc24f2ea0f1e9c88b287c7f47
lephleg <lephleg@gmail.com>
2017-12-30 22:37:10 +0200

    Add empty country list fragment, Add dummy search options menu item, Add settings options menu item, Define settings activity and general preferences.

---
+++ app/src/main/java/com/example/lephleg/worldliving/CountryListFragment.java
@@ -0,0 +1,33 @@
+package com.example.lephleg.worldliving;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CountryListFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.search_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.country_list_fragment, container, false);
+        return rootView;
+    }
+
+}
---
+++ app/src/main/java/com/example/lephleg/worldliving/SettingsActivity.java
@@ -0,0 +1,14 @@
+package com.example.lephleg.worldliving;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class SettingsActivity extends PreferenceActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.pref_general);
+    }
+
+}
