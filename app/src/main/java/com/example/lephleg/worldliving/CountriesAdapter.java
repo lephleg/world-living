291808490ca8c19c0c5234030dd01bf1727ad86d
lephleg <lephleg@gmail.com>
2018-01-05 15:17:00 +0200

    Implement view holder pattern for list view.

--- app/src/main/java/com/example/lephleg/worldliving/CountriesAdapter.java
+++ app/src/main/java/com/example/lephleg/worldliving/CountriesAdapter.java
@@ -17,6 +17,12 @@ public class CountriesAdapter extends ArrayAdapter<Country> {
     private Context mContext;
     private ArrayList<Country> mCountries;
 
+    // View lookup cache
+    private static class ViewHolder {
+        TextView name;
+        ImageView flag;
+    }
+
     public CountriesAdapter(Context context, ArrayList<Country> countries) {
         super(context,0, countries);
         this.mContext = context;
@@ -30,18 +36,26 @@ public class CountriesAdapter extends ArrayAdapter<Country> {
         Country country = mCountries.get(position);
         country.loadFlagByCode(mContext);
 
+        ViewHolder viewHolder; // view lookup cache stored in tag
+
         // Check if an existing view is being reused, otherwise inflate the view
         if (convertView == null) {
+
+            // If there's no view to re-use, inflate a brand new view for row
+            viewHolder = new ViewHolder();
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.country_item, parent, false);
+            viewHolder.name = (TextView) convertView.findViewById(R.id.country_name);
+            viewHolder.flag = (ImageView) convertView.findViewById(R.id.country_icon);
+            // Cache the viewHolder object inside the fresh view
+            convertView.setTag(viewHolder);
+        } else {
+            // View is being recycled, retrieve the viewHolder object from tag
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
-        // Lookup view for data population
-        TextView tvName = (TextView) convertView.findViewById(R.id.country_name);
-        ImageView ivFlag = (ImageView) convertView.findViewById(R.id.country_icon);
-
         // Populate the data into the template view using the data object
-        tvName.setText(country.name);
-        ivFlag.setImageResource(country.flag);
+        viewHolder.name.setText(country.name);
+        viewHolder.flag.setImageResource(country.flag);
 
         // Return the completed view to render on screen
         return convertView;
