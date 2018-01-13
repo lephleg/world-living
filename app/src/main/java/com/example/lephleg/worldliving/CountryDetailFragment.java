1f749ae18723f566d3bb348c79abc0b727334c15
lephleg <lephleg@gmail.com>
2018-01-13 15:36:55 +0200

    Fix share functionality.

--- app/src/main/java/com/example/lephleg/worldliving/CountryDetailFragment.java
+++ app/src/main/java/com/example/lephleg/worldliving/CountryDetailFragment.java
@@ -1,13 +1,12 @@
 package com.example.lephleg.worldliving;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v4.app.Fragment;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.ShareActionProvider;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -16,7 +15,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ExpandableListView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -63,11 +61,21 @@ public class CountryDetailFragment extends Fragment implements LoaderManager.Loa
         // Inflate the menu; this adds items to the action bar if it is present.
         inflater.inflate(R.menu.country_detail_fragment, menu);
 
-        // Retrieve the share menu item
+        // Locate MenuItem with ShareActionProvider
         MenuItem menuItem = menu.findItem(R.id.action_share);
 
-        // TODO: share functionality
+        // Fetch and store ShareActionProvider
+        mShareActionProvider =  (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        mShareActionProvider.setShareIntent(createShareIntent());
 
+    }
+
+    private Intent createShareIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, String.format(getContext().getResources().getString(R.string.share_text), mCountry.name));
+        return shareIntent;
     }
 
     @Override
