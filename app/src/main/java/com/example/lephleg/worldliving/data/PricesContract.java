4808034782b9868c7b841ce22acb64ed30a56234
lephleg <lephleg@gmail.com>
2018-01-14 14:23:49 +0200

    Fix content authority and column visibility.

--- app/src/main/java/com/example/lephleg/worldliving/data/PricesContract.java
+++ app/src/main/java/com/example/lephleg/worldliving/data/PricesContract.java
@@ -6,7 +6,7 @@ import android.provider.BaseColumns;
 
 public class PricesContract {
 
-    public static final String CONTENT_AUTHORITY = "com.example.lephleg.worldliving.data";
+    public static final String CONTENT_AUTHORITY = "com.example.lephleg.worldliving";
 
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
 
@@ -20,41 +20,41 @@ public class PricesContract {
         public static final String CONTENT_ITEM_TYPE =
                 ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PRICES;
 
-        static final String TABLE_NAME = "prices";
+        public static final String TABLE_NAME = "prices";
 
-        static final String COLUMN_ID = "id";
-        static final String COLUMN_COUNTRY_CODE = "country_code";
-        static final String COLUMN_CURRENCY_CODE = "currency_code";
+        public static final String COLUMN_ID = "id";
+        public static final String COLUMN_COUNTRY_CODE = "country_code";
+        public static final String COLUMN_CURRENCY_CODE = "currency_code";
 
-        static final String COLUMN_MEAL = "meal";
-        static final String COLUMN_WATER = "water";
-        static final String COLUMN_COFFEE = "coffee";
-        static final String COLUMN_COKE = "coke";
-        static final String COLUMN_MILK = "milk";
-        static final String COLUMN_BREAD = "bread";
-        static final String COLUMN_RICE = "rice";
-        static final String COLUMN_EGGS = "eggs";
-        static final String COLUMN_CHICKEN = "chicken";
-        static final String COLUMN_BEEF = "beef";
-        static final String COLUMN_WINE = "wine";
+        public static final String COLUMN_MEAL = "meal";
+        public static final String COLUMN_WATER = "water";
+        public static final String COLUMN_COFFEE = "coffee";
+        public static final String COLUMN_COKE = "coke";
+        public static final String COLUMN_MILK = "milk";
+        public static final String COLUMN_BREAD = "bread";
+        public static final String COLUMN_RICE = "rice";
+        public static final String COLUMN_EGGS = "eggs";
+        public static final String COLUMN_CHICKEN = "chicken";
+        public static final String COLUMN_BEEF = "beef";
+        public static final String COLUMN_WINE = "wine";
 
-        static final String COLUMN_TICKET = "ticket";
-        static final String COLUMN_TAXI_START = "taxi_start";
-        static final String COLUMN_TAXI_1KM = "taxi_1km";
-        static final String COLUMN_GAS = "gas";
+        public static final String COLUMN_TICKET = "ticket";
+        public static final String COLUMN_TAXI_START = "taxi_start";
+        public static final String COLUMN_TAXI_1KM = "taxi_1km";
+        public static final String COLUMN_GAS = "gas";
 
-        static final String COLUMN_UTILITIES = "utilities";
-        static final String COLUMN_INTERNET = "internet";
+        public static final String COLUMN_UTILITIES = "utilities";
+        public static final String COLUMN_INTERNET = "internet";
 
-        static final String COLUMN_GYM = "gym";
-        static final String COLUMN_CINEMA = "cinema";
+        public static final String COLUMN_GYM = "gym";
+        public static final String COLUMN_CINEMA = "cinema";
 
-        static final String COLUMN_RENT_SM_IN = "rent_sm_in";
-        static final String COLUMN_RENT_SM_OUT = "rent_sm_out";
-        static final String COLUMN_RENT_MD_IN = "rent_md_in";
-        static final String COLUMN_RENT_MD_OUT = "rent_md_out";
+        public static final String COLUMN_RENT_SM_IN = "rent_sm_in";
+        public static final String COLUMN_RENT_SM_OUT = "rent_sm_out";
+        public static final String COLUMN_RENT_MD_IN = "rent_md_in";
+        public static final String COLUMN_RENT_MD_OUT = "rent_md_out";
 
-        static final String COLUMN_SALARY = "salary";
+        public static final String COLUMN_SALARY = "salary";
 
         public static Uri buildPricesWithCountryAndCurrency(String country, String currency) {
             return CONTENT_URI.buildUpon().appendPath(country)
